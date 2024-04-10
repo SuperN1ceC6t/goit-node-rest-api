1f28fa8c76013ac62b0d81c7c0c66ac7a132ea65
SuperNiceCat <131773661+SuperN1ceC6t@users.noreply.github.com>
2024-04-10 20:33:21 +0300

    First Commit

---
+++ controllers/authControllers.js
@@ -0,0 +1,61 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import authServices from "../services/authServices.js";
+import ctrlWrapper from "../decorators/ctrlWrapper.js";
+import HttpError from "../helpers/HttpError.js";
+
+const { JWT_SECRET } = process.env;
+
+const signup = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await authServices.findUser({ email });
+  if (user) throw HttpError(409, "Email already in use");
+  const hashPassword = await bcrypt.hash(password, 10);
+  const newUser = await authServices.signup({
+    ...req.body,
+    password: hashPassword,
+  });
+  res.status(201).json({
+    user: {
+      email: newUser.email,
+      subscription: newUser.subscription,
+    },
+  });
+};
+
+const signin = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await authServices.findUser({ email });
+  if (!user) throw HttpError(401, "Email or password is wrong");
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) throw HttpError(401, "Email or password is wrong");
+  const { _id: id } = user;
+  const payload = { id };
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "12h" });
+  await authServices.updateUser({ _id: id }, { token });
+  res.json({
+    token,
+    user: {
+      email: user.email,
+      subscription: user.subscription,
+    },
+  });
+};
+
+const getCurrent = async (req, res) => {
+  const { email, subscription } = req.user;
+  res.json({ email, subscription });
+};
+
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await authServices.updateUser({ _id }, { token: null });
+  res.status(204).send();
+};
+
+export default {
+  signup: ctrlWrapper(signup),
+  signin: ctrlWrapper(signin),
+  getCurrent: ctrlWrapper(getCurrent),
+  logout: ctrlWrapper(logout),
+};
---
+++ middlewares/authenticate.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import HttpError from "../helpers/HttpError.js";
+import authServices from "../services/authServices.js";
+
+const { JWT_SECRET } = process.env;
+
+const authenticate = async (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) return next(HttpError(401, "Not authorized"));
+  const [bearer, token] = authorization.split(" ");
+  if (bearer !== "Bearer") return next(HttpError(401, "Not authorized"));
+  try {
+    const { id } = jwt.verify(token, JWT_SECRET);
+    const user = await authServices.findUser({ _id: id });
+    if (!user) return next(HttpError(401, "User not found"));
+    if (!user.token) return next(HttpError(401, "Not authorized"));
+    req.user = user;
+    next();
+  } catch (error) {
+    next(HttpError(401, "Not authorized"));
+  }
+};
+
+export default authenticate;
